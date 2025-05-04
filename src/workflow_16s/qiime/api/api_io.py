bd81780fce3b70074d22b26d841f8ef9f949e62b
Heather MacGregor <50675985+heathermacgregor@users.noreply.github.com>
2025-05-04 11:31:35 -0700

    Update api_io.py

--- src/workflow_16s/qiime/api/api_io.py
+++ src/workflow_16s/qiime/api/api_io.py
@@ -10,7 +10,7 @@ from qiime2 import Artifact
 
 # ================================= DEFAULT VALUES =================================== #
 
-DEFAULT_N = 20
+DEFAULT_N = 15
 
 # Set environment variable and suppress warnings
 os.environ["TMPDIR"] = "/opt/tmp"
@@ -61,7 +61,7 @@ def load_with_print(
     """Constructs a file path and loads a QIIME 2 artifact from it."""
     file_path = str(construct_file_path(file_dir, prefix, suffix))
     artifact = Artifact.load(file_path)
-    print(f"{'    Loaded from':{n}.{n}}: {file_path}")
+    print(f"{'    ✅ Loaded from':{n}.{n}}: {file_path}")
     return artifact
 
 
@@ -75,7 +75,7 @@ def save_with_print(
     """"""
     file_path = str(construct_file_path(file_dir, prefix, suffix))
     artifact.save(file_path)
-    print(f"{'    Saved to':{n}.{n}}: {file_path}")
+    print(f"{'    ✅ Saved to':{n}.{n}}: {file_path}")
 
 
 def export_with_print(
@@ -90,7 +90,7 @@ def export_with_print(
     dir_path = str(file_path).strip(suffix).strip(".")
     Path(dir_path).mkdir(parents=True, exist_ok=True)
     artifact.export_data(str(dir_path))
-    print(f"{'    Exported to':{n}.{n}}: {file_path}")
+    print(f"{'    ✅ Exported to':{n}.{n}}: {dir_path}")
 
 
 def save_and_export_with_print(
