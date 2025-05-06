395d7e1d1b61c8288348238cc3ad36b02818640b
Heather MacGregor <50675985+heathermacgregor@users.noreply.github.com>
2025-05-05 22:49:47 -0700

    Update utils.py

--- src/workflow_16s/stats/utils.py
+++ src/workflow_16s/stats/utils.py
@@ -28,6 +28,113 @@ from tqdm import tqdm
 
 # ==================================== FUNCTIONS ===================================== #
 
+def filter_table(
+    table: Union[Dict, Table, pd.DataFrame], 
+    min_rel_abundance: float = 1,
+    min_samples: int = 10,
+    min_counts: int = 1000
+) -> pd.DataFrame:
+    """"""
+    # Convert table to DataFrame if necessary
+    if isinstance(table, Table):
+        table = table.to_dataframe(dense=True)
+        # Transpose it (samples x features)
+        table = table.T
+    if isinstance(table, Dict):
+        table = pd.DataFrame(table)
+    
+    table = filter_features(table, min_rel_abundance, min_samples)
+    table = feature_samples(table, min_counts)
+    return table
+
+
+def filter_features(
+    table: pd.DataFrame,
+    min_rel_abundance: float = 1,
+    min_samples: int = 10
+) -> pd.DataFrame:
+    """Filter for columns (samples) where at least one row (OTU) has a relative abundance 
+    of at least X% and for rows (OTUs) that are present in at least Y columns (samples)"""
+    table = table.loc[:, table.max(axis=0) >= min_rel_abundance / 100]           
+    table = table.loc[(table > 0).sum(axis=1) > min_samples, :]   
+    return table
+
+
+def filter_samples(
+    table: pd.DataFrame, 
+    min_counts: int = 1000
+) -> pd.DataFrame:                  
+    """Filter for columns (samples) that have at least X counts total""" 
+    return table.loc[:, (table.sum(axis=0) > min_counts)] 
+
+
+def preprocess_table(
+    table: Union[Dict, Table, pd.DataFrame], 
+    normalize: bool = True,
+    clr_transform: bool = True
+) -> pd.DataFrame:
+    """"""
+    # Convert table to DataFrame if necessary
+    if isinstance(table, Table):
+        table = table.to_dataframe(dense=True)
+        # Transpose it (samples x features)
+        table = table.T
+    if isinstance(table, Dict):
+        table = pd.DataFrame(table)
+    
+    if normalize:
+        table = normalize_table(table, axis=0)
+    if clr_transform:
+        table = clr_transform_table(table)
+    return table
+    
+    
+def normalize_table(
+    table: pd.DataFrame, 
+    axis: int = 0
+) -> pd.DataFrame:
+    """Normalize by column (sample) to get relative abundances for each sample"""
+    return table.apply(lambda x: x / x.sum(), axis=axis)
+
+
+def clr_transform_table(
+    table: pd.DataFrame
+) -> pd.DataFrame:
+    """"""
+    np_clr = CLR(table + 0.00001)  
+    return pd.DataFrame(np_clr, index=table.index, columns=table.columns)
+
+
+def presence_absence(
+    table: Union[Dict, Table, pd.DataFrame],
+    threshold: float = 0.99
+) -> pd.DataFrame:
+    """"""
+    # Convert table to DataFrame if necessary
+    if isinstance(table, Table):
+        table = table.to_dataframe(dense=True)
+        # Transpose it (samples x features)
+        table = table.T
+    if isinstance(table, Dict):
+        table = pd.DataFrame(table)
+    # Transpose back
+    table = table.T
+    # Get total counts per feature
+    feature_sums = np.array(table.sum(axis='observation')).flatten() 
+    # Sort features from most to least abundant 
+    sorted_idx = np.argsort(feature_sums)[::-1] 
+    # Get cumulative proportion of total counts contributed by the top features
+    cumulative = np.cumsum(feature_sums[sorted_idx]) / feature_sums.sum() 
+    # Get the smallest number of top features needed to account for ≥99% of the total counts
+    stop_idx = np.searchsorted(cumulative, threshold) + 1 
+    keep_ids = [table.ids(axis='observation')[i] for i in sorted_idx[:stop_idx]]
+    # Convert to presence/absence
+    pa_table = table.pa(inplace=False) 
+    filtered_table_table = pa_table.filter(keep_ids, axis='observation')
+    filtered_table = filtered_table_table.to_dataframe(dense=True)
+    return filtered_table
+
+
 def k_means(
     table: Union[Dict, Table, pd.DataFrame], 
     metadata: pd.DataFrame, 
@@ -35,6 +142,7 @@ def k_means(
     n_clusters: int = 10, 
     random_state: int = 0
 ):
+    """"""
     # Convert table to DataFrame if necessary
     if isinstance(table, Table):
         table = table.to_dataframe(dense=True)
@@ -51,13 +159,14 @@ def k_means(
     table_with_col['kmeans_cluster'] = kmeans.labels_
     return table_with_col
 
-
+# Statistical tests
 def t_test(
     table: Union[Dict, Table, pd.DataFrame], 
     metadata: pd.DataFrame,
     col: str = 'nuclear_contamination_status',
     col_values: List[Union[Bool, int, str]] = [True, False]
 ) -> pd.DataFrame:
+    """"""
     # Convert table to DataFrame if necessary
     if isinstance(table, Table):
         table = table.to_dataframe(dense=True)
@@ -90,6 +199,7 @@ def mwu_bonferroni(
     col: str = 'nuclear_contamination_status',
     col_values: List[Union[Bool, int, str]] = [True, False]
 ) -> pd.DataFrame:
+    """"""
     # Convert table to DataFrame if necessary
     if isinstance(table, Table):
         table = table.to_dataframe(dense=True)
@@ -154,18 +264,16 @@ def variability_explained(
     metadata: pd.DataFrame,
     col: str = 'nuclear_contamination_status'    
 ) -> pd.DataFrame:
-    '''
-    Calculate the variability explained by a metadata column for each feature in the data.
+    """Calculate the variability explained by a metadata column for each feature in the data
 
-    Parameters:
-        df (pd.DataFrame): DataFrame of OTU abundances.
-        metadata (pd.DataFrame): DataFrame with sample metadata.
-        col (str): Column in metadata used as the explanatory variable.
-        output_dir (Union[str, Path]): Directory to save the results.
+    Args:
+        table: DataFrame of OTU abundances
+        metadata: DataFrame with sample metadata
+        col: Column in metadata used as the explanatory variable
     
     Returns:
-        pd.DataFrame: DataFrame with variability explained (R^2) for each feature.
-    '''
+        DataFrame with variability explained (R^2) for each feature
+    """
     # Convert table to DataFrame if necessary
     if isinstance(table, Table):
         table = table.to_dataframe(dense=True)
