94868052ac504f4e1528c17ce67cf2839c460571
Heather MacGregor <50675985+heathermacgregor@users.noreply.github.com>
2025-05-22 15:12:25 -0700

    Update file_utils.py

--- src/workflow_16s/utils/file_utils.py
+++ src/workflow_16s/utils/file_utils.py
@@ -17,7 +17,7 @@ import logging
 
 from workflow_16s.utils.dir_utils import SubDirs
 from workflow_16s.stats.utils import t_test
-
+from workflow_16s.utils import df_utils
 from workflow_16s.figures.html_report import HTMLReport
 from workflow_16s.figures.merged.merged import sample_map_categorical, pcoa, pca, mds
 from workflow_16s.stats import beta_diversity 
