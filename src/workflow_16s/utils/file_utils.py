d4cb61446e1ef0094002c2d54fbd75cd557af0b8
Heather MacGregor <50675985+heathermacgregor@users.noreply.github.com>
2025-05-25 22:51:05 -0700

    Update file_utils.py

--- src/workflow_16s/utils/file_utils.py
+++ src/workflow_16s/utils/file_utils.py
@@ -38,8 +38,9 @@ from workflow_16s.utils import df_utils
 from workflow_16s.utils.dir_utils import SubDirs
 from workflow_16s.stats import beta_diversity 
 from workflow_16s.stats.utils import (
-    preprocess_table, mwu_bonferroni, kruskal_bonferroni, t_test
+    preprocess_table, mwu_bonferroni, kruskal_bonferroni
 )
+from workflow_16s.stats.parametric_tests import t_test
 from workflow_16s.figures.html_report import HTMLReport
 from workflow_16s.figures.merged.merged import (
     sample_map_categorical, pcoa, pca, mds
