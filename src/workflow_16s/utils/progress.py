83c6794c271382fae7acc7da41bd05d84d84f880
Heather MacGregor <50675985+heathermacgregor@users.noreply.github.com>
2025-06-30 12:49:28 -0700

    Update progress.py

--- src/workflow_16s/utils/progress.py
+++ src/workflow_16s/utils/progress.py
@@ -28,6 +28,13 @@ warnings.filterwarnings("ignore") # Suppress warnings
 
 DEFAULT_PROGRESS_TEXT_N = 65
 
+DEFAULT_DESCRIPTION_STYLE = "white"
+DEFAULT_BAR_COLUMN_COMPLETE_STYLE = "honeydew2"
+DEFAULT_FINISHED_STYLE = "dark_cyan" 
+DEFAULT_PROGRESS_PERCENTAGE_STYLE = "honeydew2"
+DEFAULT_TIME_ELAPSED_STYLE = "light_sky_blue1"
+DEFAULT_TIME_REMAINING_STYLE = "thistle1"
+
 # ==================================== FUNCTIONS ===================================== #
 
 def create_progress() -> Progress:
@@ -72,6 +79,60 @@ class MofNCompleteColumn(ProgressColumn):
             style="cyan",
             justify="right"
         )
+
+class TimeElapsedColumn(ProgressColumn):
+    """Renders time elapsed."""
+    
+    def render(self, task: "Task") -> Text:
+        """Show time elapsed."""
+        elapsed = task.finished_time if task.finished else task.elapsed
+        if elapsed is None:
+            return Text("-:--:--", style=DEFAULT_TIME_ELAPSED_STYLE)
+        delta = timedelta(seconds=max(0, int(elapsed)))
+        return Text(str(delta), style=DEFAULT_TIME_ELAPSED_STYLE)
+
+class TimeRemainingColumn(ProgressColumn):
+    """Renders estimated time remaining."""
+
+    # Only refresh twice a second to prevent jitter
+    max_refresh = 0.5
+
+    def __init__(
+        self,
+        compact: bool = False,
+        elapsed_when_finished: bool = False,
+        table_column: Optional[Column] = None,
+    ):
+        self.compact = compact
+        self.elapsed_when_finished = elapsed_when_finished
+        super().__init__(table_column=table_column)
+
+
+    def render(self, task: "Task") -> Text:
+        """Show time remaining."""
+        if self.elapsed_when_finished and task.finished:
+            task_time = task.finished_time
+            style = DEFAULT_TIME_ELAPSED_STYLE
+        else:
+            task_time = task.time_remaining
+            style = DEFAULT_TIME_REMAINING_STYLE
+
+        if task.total is None:
+            return Text("", style=style)
+
+        if task_time is None:
+            return Text("--:--" if self.compact else "-:--:--", style=style)
+
+        # Based on https://github.com/tqdm/tqdm/blob/master/tqdm/std.py
+        minutes, seconds = divmod(int(task_time), 60)
+        hours, minutes = divmod(minutes, 60)
+
+        if self.compact and not hours:
+            formatted = f"{minutes:02d}:{seconds:02d}"
+        else:
+            formatted = f"{hours:d}:{minutes:02d}:{seconds:02d}"
+
+        return Text(formatted, style=style)
         
 
 def get_progress_bar(transient: bool = False) -> Progress:
@@ -79,28 +140,29 @@ def get_progress_bar(transient: bool = False) -> Progress:
     return Progress(
         SpinnerColumn(
             "dots", 
-            style="bright_green", 
+            style=DEFAULT_BAR_COLUMN_COMPLETE_STYLE, 
             speed=0.75
         ),
         TextColumn(
-            "[white]{task.description}".ljust(DEFAULT_PROGRESS_TEXT_N), 
+            task.description.ljust(DEFAULT_PROGRESS_TEXT_N), 
+            style=DEFAULT_DESCRIPTION_STYLE,
             justify="left"
         ),
         MofNCompleteColumn(),
         BarColumn(
             bar_width=40,
             style="black", # Background color
-            complete_style="bright_yellow",
-            finished_style="green",
+            complete_style=DEFAULT_BAR_COLUMN_COMPLETE_STYLE,
+            finished_style=DEFAULT_FINISHED_STYLE,
             #pulse_style="yellow"
         ),
         TextColumn(
             "[progress.percentage]{task.percentage:>3.0f}%".rjust(5), 
-            style="bright_green"
+            style=DEFAULT_PROGRESS_PERCENTAGE_STYLE
         ),
         TextColumn(
             "E".rjust(2), 
-            style="yellow"
+            style=DEFAULT_TIME_ELAPSED_STYLE
         ),
         TimeElapsedColumn(),
         TextColumn(
