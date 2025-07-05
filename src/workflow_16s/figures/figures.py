f1fcbae857b46c41062774832bf09afa3aa931b7
Heather MacGregor <50675985+heathermacgregor@users.noreply.github.com>
2025-07-05 13:51:52 -0700

    Update figures.py

--- src/workflow_16s/figures/figures.py
+++ src/workflow_16s/figures/figures.py
@@ -14,6 +14,7 @@ import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.io as pio
+from plotly.subplots import make_subplots
 
 # ========================== INITIALIZATION & CONFIGURATION ========================== #
 
