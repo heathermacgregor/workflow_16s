0fe8a89d9e1343eb5fcb2596ff54b39951014284
Heather MacGregor <50675985+heathermacgregor@users.noreply.github.com>
2025-07-10 13:44:06 -0700

    Update tests.py

--- src/workflow_16s/stats/tests.py
+++ src/workflow_16s/stats/tests.py
@@ -66,6 +66,7 @@ def alpha_diversity(
         DataFrame with alpha diversity values (samples x metrics).
     """
     df = table_to_df(table)
+    print(df.index)
     results = pd.DataFrame(index=df.index)
     
     # Precompute common statistics vectorially
