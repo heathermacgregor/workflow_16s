cdcfcf06d82bff5056cb2da908dc39a3b41f97ba
Heather MacGregor <50675985+heathermacgregor@users.noreply.github.com>
2025-07-22 13:55:11 -0700

    Update analysis.py

--- src/workflow_16s/amplicon_data/analysis.py
+++ src/workflow_16s/amplicon_data/analysis.py
@@ -26,36 +26,8 @@ from workflow_16s.amplicon_data.helpers import _init_dict_level, _ProcessingMixi
 from workflow_16s.amplicon_data.maps import Maps
 from workflow_16s.amplicon_data.preprocessing import _DataLoader, _TableProcessor
 from workflow_16s.amplicon_data.statistical_analyses import run_statistical_tests_for_group
-from workflow_16s.figures.merged import (
-    mds as plot_mds,
-    pca as plot_pca,
-    pcoa as plot_pcoa,
-    create_alpha_diversity_boxplot, create_alpha_diversity_stats_plot,
-    create_feature_abundance_map, plot_alpha_correlations, sample_map_categorical, 
-    violin_feature
-)
 from workflow_16s.function.faprotax import (
     faprotax_functions_for_taxon, get_faprotax_parsed
-)
-from workflow_16s.models.feature_selection import (
-    catboost_feature_selection,
-)
-from workflow_16s.stats.beta_diversity import (
-    pcoa as calculate_pcoa,
-    pca as calculate_pca,
-    tsne as calculate_tsne,
-    umap as calculate_umap,
-)
-from workflow_16s.stats.tests import (
-    alpha_diversity, analyze_alpha_diversity, analyze_alpha_correlations,
-    fisher_exact_bonferroni, kruskal_bonferroni, mwu_bonferroni, ttest
-)
-from workflow_16s.utils.data import (
-    clr, collapse_taxa, filter, normalize, presence_absence, table_to_df, 
-    update_table_and_meta, to_biom
-)
-from workflow_16s.utils.io import (
-    export_h5py, import_merged_metadata_tsv, import_merged_table_biom
 )
 from workflow_16s.utils.progress import get_progress_bar, _format_task_desc
 from workflow_16s.utils.nfc_facilities import find_nearby_nfc_facilities
@@ -77,7 +49,7 @@ class AmpliconData:
         self, 
         cfg: Dict, 
         project_dir: Any, 
-        mode: str = DEFAULT_MODE, 
+        mode: str = constants.DEFAULT_MODE, 
         existing_subsets: Optional[Dict[str, Dict[str, Path]]] = None,
         verbose: bool = False
     ):
