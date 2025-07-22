57d136804a80a3b0022e02c95013589ee79b3d91
Heather MacGregor <50675985+heathermacgregor@users.noreply.github.com>
2025-07-22 13:44:16 -0700

    Create maps.py

---
+++ src/workflow_16s/amplicon_data/maps.py
@@ -0,0 +1,89 @@
+# ===================================== IMPORTS ====================================== #
+
+# Standard Library Imports
+import logging
+from pathlib import Path
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union
+
+# Third‑Party Imports
+import pandas as pd
+
+# ================================== LOCAL IMPORTS =================================== #
+
+from workflow_16s import constants
+from workflow_16s.figures.merged import sample_map_categorical
+from workflow_16s.utils.progress import get_progress_bar, _format_task_desc
+
+# ========================== INITIALISATION & CONFIGURATION ========================== #
+
+logger = logging.getLogger("workflow_16s")
+
+# ================================= DEFAULT VALUES =================================== #
+
+class Maps:
+    """Generates sample map plots and stores them internally"""
+    def __init__(
+        self, 
+        config: Dict, 
+        meta: pd.DataFrame,
+        output_dir: Path, 
+        verbose: bool = False
+    ):
+        self.config = config
+        self.meta = meta
+        self.output_dir = output_dir
+        self.verbose = verbose
+      
+        self.maps_config = self.config['maps']
+        self.color_columns = self.maps_config.get(
+            'color_columns',
+            [
+                constants.DEFAULT_DATASET_COLUMN,
+                constants.DEFAULT_GROUP_COLUMN,
+                "env_feature",
+                "env_material",
+                "country",
+            ],
+        )
+        
+        self.figures: Dict[str, Any] = {}
+
+    def generate_sample_maps(
+        self, 
+        nfc_facility_data: Optional[pd.DataFrame] = None,
+        **kwargs
+    ) -> Dict[str, Any]:
+        if self.maps_config.get('enabled', False):
+            return {}
+
+        meta = self.meta
+        valid_columns = [col for col in self.color_columns if col in meta]
+        missing = set(self.color_columns) - set(valid_columns)
+        if missing and self.verbose:
+            logger.warning(f"Missing columns in metadata: {', '.join(missing)}")
+
+        meta = meta.set_index('#sampleid')
+      
+        with get_progress_bar() as progress:
+            plot_desc = f"Plotting sample maps"
+            plot_task = progress.add_task(
+              _format_task_desc(plot_desc), 
+              total=len(valid_columns)
+            )
+
+            for col in valid_columns:
+                col_desc = f"Plotting sample maps → {col}"
+                progress.update(plot_task, description=_format_task_desc(col_desc))
+                
+                self.figures[col], _ = sample_map_categorical(
+                    metadata=meta,
+                    nfc_facilities_data=nfc_facility_data,
+                    output_dir=self.output_dir,
+                    color_col=col,
+                    **kwargs,
+                )
+                
+                progress.update(plot_task, advance=1)
+            progress.update(plot_task, description=_format_task_desc(plot_desc))
+        return self.figures
+      
