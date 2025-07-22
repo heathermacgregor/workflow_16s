609b18c69c01c8a82c31a8a409e333952ced2267
Heather MacGregor <50675985+heathermacgregor@users.noreply.github.com>
2025-07-22 11:33:31 -0700

    Update constants.py

--- src/workflow_16s/constants.py
+++ src/workflow_16s/constants.py
@@ -1,18 +1,12 @@
 # ================================= DEFAULT VALUES =================================== #
+
 DEFAULT_PROGRESS_TEXT_N = 65
 DEFAULT_N = 65
-DEFAULT_DATASET_COLUMN = "dataset_name"
-DEFAULT_GROUP_COLUMN = "nuclear_contamination_status"
-DEFAULT_SYMBOL_COL = DEFAULT_GROUP_COLUMN
+
+DEFAULT_DATASET_COLUMN = 'dataset_name'
+
+DEFAULT_GROUP_COLUMN = 'nuclear_contamination_status'
 DEFAULT_GROUP_COLUMN_VALUES = [True, False]
-DEFAULT_MODE = 'genus'
-DEFAULT_ALPHA_METRICS = [
-    'shannon', 'observed_features', 'simpson',
-    'pielou_evenness', 'chao1', 'ace', 
-    'gini_index', 'goods_coverage', 'heip_evenness', 
-    'dominance'       
-]
-PHYLO_METRICS = ['faith_pd', 'pd_whole_tree']
 DEFAULT_GROUP_COLUMNS = [
     {
         'name': "nuclear_contamination_status",
@@ -20,6 +14,15 @@ DEFAULT_GROUP_COLUMNS = [
         'values': [True, False]
     },
 ]
+
+DEFAULT_MODE = 'genus'
+
+DEFAULT_ALPHA_METRICS = [
+    'ace', 'chao1', 'dominance', 'gini_index', 'goods_coverage', 'heip_evenness', 
+    'observed_features', 'pielou_evenness', 'shannon', 'simpson'        
+]
+PHYLO_METRICS = ['faith_pd', 'pd_whole_tree']
+
 debug_mode = False
 
 # FIGURES
@@ -27,7 +30,7 @@ DEFAULT_HEIGHT = 1000
 DEFAULT_WIDTH = 1100
 
 DEFAULT_COLOR_COL = 'dataset_name'
-DEFAULT_SYMBOL_COL = 'nuclear_contamination_status'
+DEFAULT_SYMBOL_COL = DEFAULT_GROUP_COLUMN
 
 DEFAULT_METRIC = 'braycurtis'
 
