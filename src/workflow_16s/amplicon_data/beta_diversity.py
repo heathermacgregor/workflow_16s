15f536a8f16a99ce51ab64af3f440f5d3fafc73a
Heather MacGregor <50675985+heathermacgregor@users.noreply.github.com>
2025-07-22 16:27:49 -0700

    Update beta_diversity.py

--- src/workflow_16s/amplicon_data/beta_diversity.py
+++ src/workflow_16s/amplicon_data/beta_diversity.py
@@ -18,6 +18,7 @@ from biom.table import Table
 # ================================== LOCAL IMPORTS =================================== #
 
 from workflow_16s import constants
+from workflow_16s.amplicon_data.helpers import _init_dict_level
 from workflow_16s.figures.beta_diversity import (
     pca as plot_pca,
     pcoa as plot_pcoa,
