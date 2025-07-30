862ac93530253073c255ebd862abf63254a7f195
Heather MacGregor <50675985+heathermacgregor@users.noreply.github.com>
2025-07-29 23:53:00 -0700

    Update downstream.py

--- src/workflow_16s/amplicon_data/downstream/downstream.py
+++ src/workflow_16s/amplicon_data/downstream/downstream.py
@@ -23,10 +23,8 @@ from biom.table import Table
 from workflow_16s import constants
 from workflow_16s.amplicon_data.alpha_diversity import AlphaDiversity
 from workflow_16s.amplicon_data.beta_diversity import Ordination
-from workflow_16s.amplicon_data.helpers import _init_dict_level, _ProcessingMixin
 from workflow_16s.amplicon_data.feature_selection import FeatureSelection
 from workflow_16s.amplicon_data.maps import Maps
-from workflow_16s.amplicon_data.preprocessing import _DataLoader, _TableProcessor
 from workflow_16s.amplicon_data.statistical_analyses import (
     run_statistical_tests_for_group, TopFeaturesAnalyzer
 )
@@ -35,7 +33,8 @@ from workflow_16s.function.faprotax import (
     faprotax_functions_for_taxon, get_faprotax_parsed
 )
 from workflow_16s.utils.progress import get_progress_bar, _format_task_desc
-from workflow_16s.utils.nfc_facilities import find_nearby_nfc_facilities
+from workflow_16s.amplicon_data.downstream.load import DownstreamDataLoader
+from workflow_16s.amplicon_data.downstream.table_transformations import DownstreamTableTransformations
 
 # ==================================== FUNCTIONS ===================================== #
 
@@ -45,8 +44,7 @@ umap_lock = threading.Lock()
 
 # ================================= DEFAULT VALUES =================================== #
 
-from workflow_16s.amplicon_data.downstream.load import DownstreamDataLoader
-from workflow_16s.amplicon_data.downstream.table_transformations import DownstreamTableTransformations
+
 class Downstream:
     """Main class for orchestrating 16S amplicon data analysis pipeline."""
     ModeConfig = {
@@ -61,25 +59,20 @@ class Downstream:
         existing_subsets: Optional[Dict[str, Dict[str, Path]]] = None,
         verbose: bool = False
     ):
-        self.config = config
-        
-        self.verbose = verbose
-      
-        self.project_dir = project_dir
+        self.config, self.project_dir, self.verbose = config, project_dir, verbose
         self.existing_subsets = existing_subsets
-
         self.mode = 'genus' if self.config.get("target_subfragment_mode", constants.DEFAULT_MODE) == 'any' else 'asv'
         self._validate_mode()
         
         # Initialize result containers
+        self.metadata: Dict[str, Any] = {}
+        self.tables: Dict[str, Any] = {}
         self.maps: Optional[Dict[str, Any]] = None
-        
-        self.stats: Dict[str, Any] = {}
-        self.top_features = {}
-        self.ordination: Dict[str, Any] = {}
-        self.models: Dict[str, Any] = {}
-        self.alpha_diversity: Dict[str, Any] = {}
-
+        self.stats: Optional[Dict[str, Any]] = None
+        self.alpha_diversity: Optional[Dict[str, Any]] = None
+        self.beta_diversity: Optional[Dict[str, Any]] = None
+        self.most_important_features: Optional[Dict[str, Any]] = None
+        self.catboost_models: Optional[Dict[str, Any]] = None
         
         logger.info("Running downstream analysis pipeline...")
         self._execute_pipeline()
@@ -92,34 +85,18 @@ class Downstream:
         """Execute the analysis pipeline in sequence."""
         self.metadata, self.tables, self.nfc_facilities = self._load_data()
         self.metadata, self.tables = self._process_tables()
-        print(self.metadata)
-        #self._run_analysis()
+        self._run_analysis()
         
         if self.verbose:
             logger.info("AmpliconData analysis finished.")
 
     def _load_data(self):
-        data_loader = DownstreamDataLoader(self.config, self.mode, self.project_dir, self.existing_subsets)
-        return data_loader.metadata, data_loader.tables, data_loader.nfc_facilities
+        ddl = DownstreamDataLoader(self.config, self.mode, self.project_dir, self.existing_subsets)
+        return ddl.metadata, ddl.tables, ddl.nfc_facilities
     
     def _process_tables(self):
-        dtt = DownstreamTableTransformations(
-            self.config,
-            self.tables,#Table,
-            self.metadata,#pd.DataFrame,
-            self.mode,
-            self.project_dir
-        )
+        dtt = DownstreamTableTransformations(self.config, self.tables, self.metadata, self.mode, self.project_dir)
         return dtt.metadata, dtt.tables
-    """
-        processor = _TableProcessor(
-            config=self.config,
-            mode=self.mode,
-            meta=self.meta,
-            table=self.table,
-            project_dir=self.project_dir,
-            output_dir=Path(self.project_dir.final),
-            verbose=self.verbose
-        )
-        self.tables = processor.tables
-    """
+
+    def _run_analysis(self):
+        placeholder = ''
