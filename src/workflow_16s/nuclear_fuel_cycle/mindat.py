24207247ecb13957e9ad1c286601836cfdf2b0c6
Heather MacGregor <50675985+heathermacgregor@users.noreply.github.com>
2025-09-02 10:29:14 -0700

    Use USER_AGENT constant for session headers

--- src/workflow_16s/nuclear_fuel_cycle/mindat.py
+++ src/workflow_16s/nuclear_fuel_cycle/mindat.py
@@ -9,7 +9,7 @@ from openmindat import LocalitiesRetriever
 from pathlib import Path
 from typing import Union
 
-from workflow_16s.constants import MINDAT_API_KEY, REFERENCES_DIR 
+from workflow_16s.constants import MINDAT_API_KEY, REFERENCES_DIR, USER_AGENT 
 
 def gpd_from_df(df):
     return gpd.GeoDataFrame(
@@ -25,9 +25,7 @@ class MinDatScraper:
       
     def _get_session(self):
         self.session = requests.Session()
-        self.session.headers.update({
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-        })
+        self.session.headers.update({'User-Agent': USER_AGENT})
       
     def _get_soup(self, url):
         response = self.session.get(url)
