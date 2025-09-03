c7f5d6e9cc1adff79bc60bf913cc8b1f8680f9f2
Heather MacGregor <50675985+heathermacgregor@users.noreply.github.com>
2025-09-03 12:48:36 -0700

    Update database loading for mindat and wikipedia

--- src/workflow_16s/nuclear_fuel_cycle/nuclear_fuel_cycle.py
+++ src/workflow_16s/nuclear_fuel_cycle/nuclear_fuel_cycle.py
@@ -117,9 +117,14 @@ class NFCFacilitiesHandler:
             database_dfs.append(other_databases.load_nfc_facilities(config=self.config, output_dir=self.output_dir))
         if "MinDat" in self.database_names:
             mindat_results = mindat.world_uranium_mines(self.mindat_api_key, self.output_dir)
-            database_dfs.append(mindat_results[0])
+            database_dfs.append(mindat_results[0][[
+                "facility", "country", "latitude", "longitude", "elements", "refs", "wikipedia", "data_source"
+            ]])
         if "Wikipedia" in self.database_names:
-            database_dfs.append(wikipedia.world_nfc_facilities(output_dir=self.output_dir))
+            wikipedia_results = wikipedia.world_nfc_facilities(output_dir=self.output_dir)[[
+                "facility", "country", "facility_start_year", "facility_end_year", "lat_lon", "data_source"
+            ]]
+            database_dfs.append(wikipedia_results)
         
         #database_dfs = [item for sublist in database_dfs for item in sublist]
         dfs = []
@@ -132,6 +137,7 @@ class NFCFacilitiesHandler:
                 dfs.append(df)  # Directly append
             
         facilities_df = pd.concat(dfs, axis=0)
+        facilities_df = facilities_df.sort_values(by='facility')
         return facilities_df
 
     def _geocode(self, df: pd.DataFrame):
